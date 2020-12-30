c21dfc7b9a14ba8d13a5a35fb8ffaf59e0388815
Kelly Harrop <kelly_harrop@intuit.com>
2020-12-30 00:26:36 -0600

    initial commit

---
+++ components/layout.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export const siteTitle = 'Kelly Harrop, 🎨 👩🏻‍💻'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className="inline-flex">
+        <div className="logo">
+          <Link href="/">
+            <a>Kelly Harrop</a>
+          </Link>
+        </div>
+        <nav>
+          <ul className="inline-flex ml-6">
+            <li>
+              <Link href="/user-guide">
+                <a>User guide</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/timeline">
+                <a>Timeline</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import { GetStaticProps } from 'next'
+import { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  return (
+    <>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section>
+        <ul>
+          {allPostsData.slice(0, 1).map(({ id, date, title }) => (
+            <li key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
